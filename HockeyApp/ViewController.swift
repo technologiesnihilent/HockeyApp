e38b18a1e69d35b6e87b8cc5fe9f050856c6884f
administrator <admin@administrators-MacBook-Pro.local>
2017-09-22 17:28:10 +0530

    Initial Commit

---
+++ HockeyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HockeyApp
+//
+//  Created by administrator  on 22/09/17.
+//  Copyright © 2017 administrator . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
