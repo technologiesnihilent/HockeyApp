0f41d8d442357edd83e59dd639026610b0076571
administrator <admin@administrators-MacBook-Pro.local>
2017-09-27 21:30:29 +0530

    curl for hockey app updated

--- deploy_hockeyapp.sh
+++ deploy_hockeyapp.sh
@@ -55,11 +55,7 @@ fi
 curl --verbose \
      --fail \
      --form "status=2" \
-     --form "notify=${CURL_NOTIFY}" \
-     --form "notes=${HOCKEYAPP_NOTES}" \
-     --form "notes_type=1" \
+     --form "notify=1" \
      --form "ipa=@/Users/distiller/HockeyApp/HockeyApp.xcarchive" \
-     --form "tags=${CURL_TAGS}" \
-     --form "release_type=2" \
      --header "X-HockeyAppToken: ${HOCKEYAPP_TOKEN}" \
      "https://rink.hockeyapp.net/api/2/apps/${HOCKEYAPP_APP_IDENTIFIER}/app_versions"
